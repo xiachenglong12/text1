701098c55277e7f4aac0146dd813d6902e6f22fb
xiacl <972471893@qq.com>
2018-04-08 12:41:53 +0800

    Initial Commit

---
+++ Text1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Text1
+//
+//  Created by 夏成龙 mac on 2018/4/8.
+//  Copyright © 2018年 夏成龙mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
