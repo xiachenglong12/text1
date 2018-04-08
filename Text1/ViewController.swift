f51293fc28cf119e9ce62b7586b50bc5b7d0a84c
xiacl <972471893@qq.com>
2018-04-08 12:43:31 +0800

    first commit

--- Text1/ViewController.swift
+++ Text1/ViewController.swift
@@ -12,7 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        title = "哈哈哈"
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
